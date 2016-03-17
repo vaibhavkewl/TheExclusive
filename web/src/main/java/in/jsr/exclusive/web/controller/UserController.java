de73f98b880a9d605ec5a42f0d8aa541ecd4d263
vaibhavkewl <vaibhavkewl@gmail.com>
2016-03-17 19:46:50 +0100

    test commit

--- web/src/main/java/in/jsr/exclusive/web/controller/UserController.java
+++ web/src/main/java/in/jsr/exclusive/web/controller/UserController.java
@@ -1,14 +1,29 @@
 package in.jsr.exclusive.web.controller;
 
+import in.jsr.exclusive.persistence.domain.User;
+import in.jsr.exclusive.persistence.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+
+    @Autowired
+    private UserRepository userRepository;
+
+
     @RequestMapping("/")
     public String index() {
-        return "Greetings from The Exclusive!";
+        System.out.println("------------> userRepository : " + userRepository);
+
+        User user = new User();
+        user.setUserName("vaibhav");
+        user.setPassword("vaibhav");
+
+        userRepository.save(user);
+        return "Greetings from The Exclusive! " + userRepository;
     }
 
 }
